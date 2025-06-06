c57b5c8c3a2fbc03659b60986e936fef50da9f52
SIDDARTH07-cyber <pvishnu@karunya.edu.in>
2025-06-06 14:00:40 +0530

    Add files via upload

---
+++ image_classification.py
@@ -0,0 +1,69 @@
+# import tensorflow as tf
+# from tensorflow.keras import layers, models
+# import matplotlib.pyplot as plt
+
+# (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+# x_train = x_train / 255.0
+# x_test = x_test / 255.0
+
+# model = models.Sequential([
+#     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+#     layers.MaxPooling2D((2, 2)),
+#     layers.Conv2D(64, (3, 3), activation='relu'),
+#     layers.MaxPooling2D((2, 2)),
+#     layers.Conv2D(64, (3, 3), activation='relu'),
+#     layers.Flatten(),
+#     layers.Dense(64, activation='relu'),
+#     layers.Dense(10, activation='softmax')
+# ])
+
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+# test_loss, test_acc = model.evaluate(x_test, y_test)
+# print("Test accuracy:", test_acc)
+
+# plt.plot(history.history['accuracy'], label='Train Accuracy')
+# plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
+# plt.legend()
+# plt.title('Training vs Validation Accuracy')
+# plt.show()
+
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(test_acc)
+
+plt.plot(history.history['accuracy'], label='train acc')
+plt.plot(history.history['val_accuracy'], label='val acc')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+model.save("cnn_model_codtech.h5")
